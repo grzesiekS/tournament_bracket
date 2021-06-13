7edde8b5da05e85ea952a9ce98dceb8a7b8933d4
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-13 12:48:06 +0200

    Add global styles

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { BracketScoreView } from './components/views/BracketScoreView/BracketScoreView';
 
+import './styles/global.scss';
+
 export const App: React.FC = () => (
   <BracketScoreView />
 );
