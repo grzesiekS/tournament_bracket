f0bd4efd05801278455bc16929fe2e825326d99e
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-13 18:00:16 +0200

    Implement BackgroundAnimation component in App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
 
 import { BracketScoreView } from './components/views/BracketScoreView/BracketScoreView';
+import { BackgroundAnimation } from './components/common/BackgroundAnimation/BackgroundAnimation';
 
 import './styles/global.scss';
 
 export const App: React.FC = () => (
-  <BracketScoreView />
+  <>
+    <BracketScoreView />
+    <BackgroundAnimation />
+  </>
 );
