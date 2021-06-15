464da83992fd7339fc2ae24bbe41294bc4d11736
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-15 18:27:37 +0200

    Remove bracket from common folder

--- src/components/common/Bracket/Bracket.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import styles from './Bracket.module.scss';
-
-export const Bracket: React.FC = () => (
-  <div className={styles.container}>
-    <h1>Bracket</h1>
-  </div>
-);
