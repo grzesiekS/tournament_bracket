3203cab7c8b4c3945764476f4b300c65579886c2
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-17 19:00:12 +0200

    Create Team component

---
+++ src/components/features/Team/Team.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import styles from './Team.module.scss';
+
+export const Team: React.FC = () => (
+  <div className={styles.container}>
+    <h1>Team</h1>
+  </div>
+);
