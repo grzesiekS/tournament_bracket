65143c65a64326d16b04f2f9ebf73fe15ccf5ccc
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-17 20:40:24 +0200

    Add ActionInt interface

--- src/common/interface.ts
+++ src/common/interface.ts
@@ -1,3 +1,9 @@
+export interface ActionInt {
+  type: string,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  payload?: any,
+}
+
 export interface TeamInt {
   name: string,
   players: string[],
