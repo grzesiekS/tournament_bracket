d068a52ac7eb859e28c2427d06fd911df5751c93
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-20 12:53:46 +0200

    implement Team component in ScoreList

--- src/components/features/Bracket/Bracket.tsx
+++ src/components/features/Bracket/Bracket.tsx
@@ -9,20 +9,14 @@ import styles from './Bracket.module.scss';
 
 export const Bracket: React.FC = () => {
   const teams = useSelector(getTeams);
-  console.log(teams);
 
   return (
     <div className={styles.container}>
       <h1>Bracket</h1>
       {teams.map(team => (
         <Team
-          _id={team._id}
-          draw={team.draw}
-          lose={team.lose}
-          win={team.win}
-          name={team.name}
-          players={team.players}
           key={team._id}
+          {...team}
         />
       ))}
     </div>
--- src/components/features/ScoreList/ScoreList.tsx
+++ src/components/features/ScoreList/ScoreList.tsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { getTeams } from '../../../redux/teamsRedux';
+
+import { Team } from '../Team/Team';
 
 import styles from './ScoreList.module.scss';
 
-export const ScoreList: React.FC = () => (
-  <div className={styles.container}>
-    <h1>Score List</h1>
-  </div>
-);
+export const ScoreList: React.FC = () => {
+  const teams = useSelector(getTeams);
+
+  return (
+    <div className={styles.container}>
+      <h1>Score List</h1>
+      {teams.map(team => (
+        <Team
+          key={team._id}
+          {...team}
+        />
+      ))}
+    </div>
+  );
+};
