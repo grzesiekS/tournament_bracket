0540ed961e814e502ef1abf346a7990ca0cdb06f
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-20 12:25:43 +0200

    Implement Team component in Bracket

--- src/components/features/Bracket/Bracket.tsx
+++ src/components/features/Bracket/Bracket.tsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+
+import { getTeams } from '../../../redux/teamsRedux';
+
+import { Team } from '../Team/Team';
 
 import styles from './Bracket.module.scss';
 
-export const Bracket: React.FC = () => (
-  <div className={styles.container}>
-    <h1>Bracket</h1>
-  </div>
-);
+export const Bracket: React.FC = () => {
+  const teams = useSelector(getTeams);
+  console.log(teams);
+
+  return (
+    <div className={styles.container}>
+      <h1>Bracket</h1>
+      {teams.map(team => (
+        <Team
+          _id={team._id}
+          draw={team.draw}
+          lose={team.lose}
+          win={team.win}
+          name={team.name}
+          players={team.players}
+          key={team._id}
+        />
+      ))}
+    </div>
+  );
+};
