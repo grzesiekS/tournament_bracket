9ddf06426b1b108b4cdeecba13a5eeffad7b6fba
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-20 12:50:22 +0200

    Add styles for Team component

--- src/components/features/Team/Team.tsx
+++ src/components/features/Team/Team.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import { TeamInt } from '../../../common/interface';
 
+import { Title } from '../../common/Title/Title';
+
 import styles from './Team.module.scss';
 
 export const Team: React.FC<TeamInt> = ({ name, win, draw, lose }) => (
   <div className={styles.container}>
-    <h1>{name}</h1>
+    <Title text={name} color='dark'/>
     <div className={styles.flexbox}>
-      <p>{win}</p>
-      <p>{draw}</p>
-      <p>{lose}</p>
+      <p>{`w:${win}`}</p>
+      <p>{`d:${draw}`}</p>
+      <p>{`l:${lose}`}</p>
     </div>
   </div>
 );
