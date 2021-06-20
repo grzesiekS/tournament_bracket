9d94ba92fd42b0cf49c614f5e7756dc57e53cea8
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-06-20 12:25:23 +0200

    Add props to Team component

--- src/components/features/Team/Team.tsx
+++ src/components/features/Team/Team.tsx
@@ -1,9 +1,16 @@
 import React from 'react';
 
+import { TeamInt } from '../../../common/interface';
+
 import styles from './Team.module.scss';
 
-export const Team: React.FC = () => (
+export const Team: React.FC<TeamInt> = ({ name, win, draw, lose }) => (
   <div className={styles.container}>
-    <h1>Team</h1>
+    <h1>{name}</h1>
+    <div className={styles.flexbox}>
+      <p>{win}</p>
+      <p>{draw}</p>
+      <p>{lose}</p>
+    </div>
   </div>
 );
