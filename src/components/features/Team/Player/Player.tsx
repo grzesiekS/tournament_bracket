723e256399fc6ec8f4c6f17ad9f4f63f99c9109c
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-07-07 23:24:35 +0200

    Create Player component

---
+++ src/components/features/Team/Player/Player.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import styles from './Player.module.scss';
+
+export const Player: React.FC = () => {
+  return(
+    <div className={styles.container}>
+      <p>Player</p>
+    </div>
+  );
+};
