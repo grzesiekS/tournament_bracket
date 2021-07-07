946454b04fd370a18f8a34dc529cb214373d41ec
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-07-07 23:37:04 +0200

    Implement Player component in Team

--- src/components/features/Team/Player/Player.tsx
+++ src/components/features/Team/Player/Player.tsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import styles from './Player.module.scss';
 
-export const Player: React.FC = () => {
+export const Player: React.FC<{id:string}> = ({ id }) => {
   return(
     <div className={styles.container}>
-      <p>Player</p>
+      <p>{id}</p>
     </div>
   );
 };
