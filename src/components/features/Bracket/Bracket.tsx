cbf0e9a21ce12d888dc949fb0d2f154831146119
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-07-24 20:37:24 +0200

    Create TeamAddPopUp components and implement it in Bracket

--- src/components/features/Bracket/Bracket.tsx
+++ src/components/features/Bracket/Bracket.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useSelector } from 'react-redux';
 
 import { getTeams } from '../../../redux/teamsRedux';
 
 import { Team } from '../Team/Team';
+import { Button } from '../../common/Button/Button';
+import { ModalBox } from '../../common/ModalBox/ModalBox';
+import { TeamAddPopUp } from './TeamAddPopUp/TeamAddPopUp';
 
 import styles from './Bracket.module.scss';
 
 export const Bracket: React.FC = () => {
   const teams = useSelector(getTeams);
 
+  const [teamAddPopUpVisibility, setTeamAddPopUpVisibility] = useState(false);
+
   return (
-    <div className={styles.container}>
-      <h1>Bracket</h1>
-      {teams.map(team => (
-        <Team
-          key={team._id}
-          {...team}
+    <ModalBox>
+      {teamAddPopUpVisibility 
+        && 
+        <TeamAddPopUp 
+          closePopUp={() => setTeamAddPopUpVisibility(false)}
         />
-      ))}
-    </div>
+      }
+      <div className={styles.container}>
+        <h1>Bracket</h1>
+        <Button 
+          title='Add team'
+          actionOnClick={() => setTeamAddPopUpVisibility(true)} 
+        />
+        {teams.map(team => (
+          <Team
+            key={team._id}
+            {...team}
+          />
+        ))}
+      </div>
+    </ModalBox>
   );
 };
