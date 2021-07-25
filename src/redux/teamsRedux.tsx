81a869c1c9fbddeef7b34140aa1b26a23e8c44b8
Grzegorz Swieboda <grzesiekswieboda@gmail.com>
2021-07-25 10:26:28 +0200

    Create AddTeam action

--- src/redux/teamsRedux.tsx
+++ src/redux/teamsRedux.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/explicit-module-boundary-types */
 interface ActionInt {
   type: string,
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
@@ -19,14 +20,31 @@ export const getTeams = (state: { teams: TeamInt[]; }):TeamInt[] => state.teams;
 /* ACTIONS */
 
 // action name creator
-// const reducerName = 'teams';
-// const createActionName = (name:string) => `app/${reducerName}/${name}`;
+const reducerName = 'teams';
+const createActionName = (name:string) => `app/${reducerName}/${name}`;
 
 // Action types
+const ADD_TEAM = createActionName('ADD_TEAM');
+
+// Action creators
+interface NewTeamPayload {
+  payload: TeamInt,
+  type: string,
+}
+export const addNewTeam = (payload: TeamInt): NewTeamPayload => ({ payload, type: ADD_TEAM });
 
 // reducer
-export default function reducer(state = [], action:ActionInt = { type: '' }):TeamInt[] {
+export default function reducer(state: { teams: never; }, action:ActionInt = { type: '' }) {
   switch(action.type) {
+    case ADD_TEAM: {
+      return {
+        ...state,
+        teams: [
+          ...state.teams,
+          action.payload,
+        ],
+      };
+    }
     default:
       return state;
   }
